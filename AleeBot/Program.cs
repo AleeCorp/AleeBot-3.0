081ca26a95c42aa3c4617e9f77ac16ecd0c96f75
Alee <Alee14498@gmail.com>
2019-05-04 21:15:23 -0400

    Add project files.

---
+++ AleeBot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Discord;
+using Discord.API;
+
+namespace AleeBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AleeBot!");
+        }
+    }
+}
